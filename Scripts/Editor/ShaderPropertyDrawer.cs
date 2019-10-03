6a00a1aa537d8c0fc6b82839309d3a23596cef53
Lachlan Sleight <lachlansleight@gmail.com>
2019-10-02 22:17:50 -0700

    Added ShaderProperty system

---
+++ Scripts/Editor/ShaderPropertyDrawer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ShaderProperty))]
+public class ShaderPropertyDrawer : PropertyDrawer
+{
+    
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return -2f;
+    }
+    
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+        
+        GUILayout.BeginVertical(GUI.skin.GetStyle("HelpBox"));
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.PropertyField(property.FindPropertyRelative("Type"), new GUIContent("Type"));
+        var index = property.FindPropertyRelative("Type").enumValueIndex;
+        if (index == 5) {
+            EditorGUILayout.PropertyField(property.FindPropertyRelative("KernelIndex"), new GUIContent("KernelIndex"));
+        }
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.PropertyField(property.FindPropertyRelative("Name"), new GUIContent("Name"));
+        switch (index) {
+            case 0:
+                EditorGUILayout.PropertyField(property.FindPropertyRelative("FloatValue"), new GUIContent("Value"));
+                break;
+            case 1:
+                EditorGUILayout.PropertyField(property.FindPropertyRelative("IntValue"), new GUIContent("Value"));
+                break;
+            case 2:
+                EditorGUILayout.PropertyField(property.FindPropertyRelative("VectorValue"), new GUIContent("Value"));
+                break;
+            case 3:
+                EditorGUILayout.PropertyField(property.FindPropertyRelative("BoolValue"), new GUIContent("Value"));
+                break;
+            case 4:
+                EditorGUILayout.PropertyField(property.FindPropertyRelative("MatrixValue"), new GUIContent("Value"));
+                break;
+            case 5:
+                EditorGUILayout.PropertyField(property.FindPropertyRelative("TextureValue"), new GUIContent("Value"));
+                break;
+        }
+        
+        EditorGUILayout.EndVertical();
+        
+        EditorGUI.EndProperty();
+    }
+}
---
+++ Scripts/ShaderProperty.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[Serializable]
+public class ShaderProperty
+{
+    public enum PropertyType
+    {
+        Float,
+        Int,
+        Vector,
+        Bool,
+        Matrix,
+        Texture,
+        Color,
+        TextureScale,
+        TextureOffset
+    }
+
+    public PropertyType Type;
+    public string Name;
+    public int KernelIndex;
+    public int IntValue;
+    public float FloatValue;
+    public Vector4 VectorValue;
+    public bool BoolValue;
+    public Matrix4x4 MatrixValue;
+    public Texture TextureValue;
+    public Color ColorValue;
+    public Vector2 TextureScaleValue;
+    public Vector2 TextureOffsetValue;
+
+}
+
+public static class ShaderPropertyExtensions
+{
+    public static void SetProperty(this ComputeShader shader, ShaderProperty property)
+    {
+        switch (property.Type) {
+            case ShaderProperty.PropertyType.Float:
+                shader.SetFloat(property.Name, property.FloatValue);
+                break;
+            case ShaderProperty.PropertyType.Int:
+                shader.SetInt(property.Name, property.IntValue);
+                break;
+            case ShaderProperty.PropertyType.Vector:
+                shader.SetVector(property.Name, property.VectorValue);
+                break;
+            case ShaderProperty.PropertyType.Bool:
+                shader.SetBool(property.Name, property.BoolValue);
+                break;
+            case ShaderProperty.PropertyType.Matrix:
+                shader.SetMatrix(property.Name, property.MatrixValue);
+                break;
+            case ShaderProperty.PropertyType.Texture:
+                shader.SetTexture(property.KernelIndex, property.Name, property.TextureValue);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    public static void SetProperty(this Material material, ShaderProperty property)
+    {
+        switch (property.Type) {
+            case ShaderProperty.PropertyType.Float:
+                material.SetFloat(property.Name, property.FloatValue);
+                break;
+            case ShaderProperty.PropertyType.Int:
+                material.SetInt(property.Name, property.IntValue);
+                break;
+            case ShaderProperty.PropertyType.Vector:
+                material.SetVector(property.Name, property.VectorValue);
+                break;
+            case ShaderProperty.PropertyType.Color:
+                material.SetColor(property.Name, property.ColorValue);
+                break;
+            case ShaderProperty.PropertyType.Matrix:
+                material.SetMatrix(property.Name, property.MatrixValue);
+                break;
+            case ShaderProperty.PropertyType.Texture:
+                material.SetTexture(property.Name, property.TextureValue);
+                break;
+            case ShaderProperty.PropertyType.TextureScale:
+                material.SetTextureScale(property.Name, property.TextureOffsetValue);
+                break;
+            case ShaderProperty.PropertyType.TextureOffset:
+                material.SetTextureOffset(property.Name, property.TextureScaleValue);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
