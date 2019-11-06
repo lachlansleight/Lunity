123f5651f1d8df20a52c40e4d9a2888739d67c8e
Lachlan Sleight <lachlansleight@gmail.com>
2019-11-06 11:35:20 +1100

    Added Min-Max Range

---
+++ Scripts/Editor/MinMaxRangePropertyDrawer.cs
@@ -0,0 +1,44 @@
+﻿using Lunity;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(MinMaxRange))]
+public class MinMaxRangePropertyDrawer : PropertyDrawer
+{
+    const float CELL_HEIGHT = 16;
+
+    Rect position;
+    SerializedProperty property;
+    GUIContent label;
+
+    // Draw the property inside the given rect
+    public override void OnGUI(Rect pos, SerializedProperty prop, GUIContent lab)
+    {
+        position = pos;
+        property = prop;
+        label = lab;
+
+        // Using BeginProperty / EndProperty on the parent property means that
+        // prefab override logic works on the entire property.
+        EditorGUI.BeginProperty(position, label, property);
+
+        // Draw label
+        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+        // Don't make child fields be indented
+        var indent = EditorGUI.indentLevel;
+        EditorGUI.indentLevel = 0;
+
+        var minRect = new Rect(pos.x + EditorGUIUtility.labelWidth, pos.y, 80f, pos.height);
+        var maxRect = new Rect(pos.x + EditorGUIUtility.labelWidth + 95f, pos.y, 100f, pos.height);
+
+        EditorGUI.PropertyField(minRect, property.FindPropertyRelative("min"), GUIContent.none);
+        EditorGUI.PropertyField(maxRect, property.FindPropertyRelative("max"), GUIContent.none);
+
+        // Set indent back to what it was
+        EditorGUI.indentLevel = indent;
+
+        EditorGUI.EndProperty();
+    }
+
+}
---
+++ Scripts/MinMaxRange.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lunity
+{
+    [Serializable]
+    public class MinMaxRange
+    {
+        public float min;
+        public float max;
+        public float range => max - min;
+
+        public MinMaxRange(float min, float max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+    }
+}
