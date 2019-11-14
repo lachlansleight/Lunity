2f896dfad06d5f2df1b727403a4b37fbbe0aedce
Lachlan Sleight <lachlansleight@gmail.com>
2019-11-14 16:14:51 +1100

    Added CopyTargetText

---
+++ Scripts/CopyTargetText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Lunity
+{
+    [RequireComponent(typeof(Text))]
+    public class CopyTargetText : MonoBehaviour
+    {
+        public Text TargetText;
+        
+        private Text _text;
+
+        private Text Text
+        {
+            get
+            {
+                if (_text == null) _text = GetComponent<Text>();
+                return _text;
+            }
+        }
+        
+        public void Update()
+        {
+            Text.text = TargetText.text;
+        }
+    }
+}
