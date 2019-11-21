5b148bcd11ce26341673b40a8c8d61776c5ec1d3
Lachlan Sleight <lachlansleight@gmail.com>
2019-11-21 15:03:07 +1100

    Added some UI stuff (to be expanded later)

---
+++ UI/Scripts/LoadingBar.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Lunity
+{
+    public class LoadingBar : MonoBehaviour
+    {
+        public float MinValue = 0f;
+        public float MaxValue = 1f;
+        
+        private Text _displayText;
+        private Image _fillImage;
+        private float _value;
+
+        private void Awake()
+        {
+            GetReferences();
+            SetValueAndText(MinValue, "");
+        }
+
+        private void GetReferences()
+        {
+            _displayText = GetComponentInChildren<Text>();
+            _fillImage = transform.Find("FillImage").GetComponent<Image>();
+        }
+
+        /// <summary>
+        /// Sets the display value for the progress bar
+        /// </summary>
+        /// <param name="value">The new value to display</param>
+        public void SetValue(float value)
+        {
+            if(_fillImage == null) GetReferences();
+            _fillImage.fillAmount = Mathf.InverseLerp(MinValue, MaxValue, value);
+            _value = value;
+        }
+
+        /// <summary>
+        /// Sets the display text for the progress bar
+        /// </summary>
+        /// <param name="text">The text to display</param>
+        public void SetText(string text)
+        {
+            if(_displayText == null) GetReferences();
+            _displayText.text = text;
+        }
+
+        /// <summary>
+        /// Sets both the value and text for the progress bar
+        /// </summary>
+        /// <param name="value">The new value to display</param>
+        /// <param name="text">The new text to display</param>
+        public void SetValueAndText(float value, string text)
+        {
+            if(_fillImage == null || _displayText == null) GetReferences();
+            _fillImage.fillAmount = Mathf.InverseLerp(MinValue, MaxValue, value);
+            _displayText.text = text;
+            _value = value;
+        }
+
+        /// <summary>
+        /// Sets the value at which the progress bar will be completely empty
+        /// </summary>
+        /// <param name="min">The new minimum value</param>
+        public void SetMinValue(float min)
+        {
+            MinValue = min;
+            SetValue(_value);
+        }
+
+        /// <summary>
+        /// Sets the value at which the progress bar will be completely full
+        /// </summary>
+        /// <param name="max">The new maximum value</param>
+        public void SetMaxValue(float max)
+        {
+            MaxValue = max;
+            SetValue(_value);
+        }
+
+        /// <summary>
+        /// Sets the boundary values for the progress bar
+        /// </summary>
+        /// <param name="min">The value at which the progress bar will be completely empty</param>
+        /// <param name="max">The value at which the progress bar will be completely full</param>
+        public void SetMinMaxValues(float min, float max)
+        {
+            MinValue = min;
+            MaxValue = max;
+            SetValue(_value);
+        }
+    }
+}
---
+++ UI/Scripts/LoadingSpinner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingSpinner : MonoBehaviour
+{
+
+    public int RingCount = 5;
+    [Space(10)]
+    public float MinSpeed = 50f;
+    public float MaxSpeed = 150f;
+    public bool AllowReverseSpeed = true;
+    [Space(10)]
+    [Range(0f, 1f)] public float MinSize = 0.3f;
+    [Space(10)] 
+    public Gradient RingColors = new Gradient();
+
+    private Transform[] _rings;
+    private float[] _speeds;
+
+    public void Awake()
+    {
+        _rings = new Transform[RingCount];
+        _speeds = new float[RingCount];
+        _rings[0] = transform.Find("Ring");
+        for (var i = 1; i < RingCount; i++) {
+            _rings[i] = Instantiate(_rings[0], transform, false);
+        }
+
+        for (var i = 0; i < RingCount; i++) {
+            var t = i / (RingCount - 1f);
+            _rings[i].localScale = Vector3.one * Mathf.Lerp(1f, MinSize, t);
+            _rings[i].GetComponent<Image>().color = RingColors.Evaluate(t);
+        }
+
+        GenerateSpeeds();
+    }
+
+    private void GenerateSpeeds()
+    {
+        for (var i = 0; i < _speeds.Length; i++) {
+            _speeds[i] = Random.Range(MinSpeed, MaxSpeed) * (AllowReverseSpeed ? Mathf.Sign(Random.Range(-1f, 1f)) : 1f);
+        }
+    }
+
+    private void Update()
+    {
+        for (var i = 0; i < _rings.Length; i++) {
+            _rings[i].Rotate(0f, 0f, _speeds[i] * Time.deltaTime, Space.Self);
+        }
+    }
+}
