97bbc1e249d21379db1b422f73c81ed32fbc74ef
Lachlan Sleight <lachlansleight@gmail.com>
2020-01-13 14:57:42 +1100

    Added VersionText

---
+++ UI/Scripts/VersionText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class VersionText : MonoBehaviour
+{
+
+    public bool PrependVersion = false;
+    
+    // Start is called before the first frame update
+    public void Awake()
+    {
+        GetComponent<Text>().text = (PrependVersion ? "Version " : "") + Application.version;
+    }
+}
