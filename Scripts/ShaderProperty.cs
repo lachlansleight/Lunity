dacf5aecf56bbe9b1155de9aec386c5be97ba5cd
Lachlan Sleight <lachlansleight@gmail.com>
2020-01-15 12:20:48 +1100

    Added property name caching for ShaderProperty

--- Scripts/ShaderProperty.cs
+++ Scripts/ShaderProperty.cs
@@ -30,6 +30,21 @@ public class ShaderProperty
     public Color ColorValue;
     public Vector2 TextureScaleValue;
     public Vector2 TextureOffsetValue;
+    
+    private int _propertyId;
+    private bool _hasPropertyId;
+
+    public int Id
+    {
+        get
+        {
+            if (_hasPropertyId) return _propertyId;
+            
+            _propertyId = Shader.PropertyToID(Name);
+            _hasPropertyId = true;
+            return _propertyId;
+        }
+    }
 
     public ShaderProperty()
     {
@@ -55,22 +70,22 @@ public static class ShaderPropertyExtensions
     {
         switch (property.Type) {
             case ShaderProperty.PropertyType.Float:
-                shader.SetFloat(property.Name, property.FloatValue);
+                shader.SetFloat(property.Id, property.FloatValue);
                 break;
             case ShaderProperty.PropertyType.Int:
-                shader.SetInt(property.Name, property.IntValue);
+                shader.SetInt(property.Id, property.IntValue);
                 break;
             case ShaderProperty.PropertyType.Vector:
-                shader.SetVector(property.Name, property.VectorValue);
+                shader.SetVector(property.Id, property.VectorValue);
                 break;
             case ShaderProperty.PropertyType.Bool:
-                shader.SetBool(property.Name, property.BoolValue);
+                shader.SetBool(property.Id, property.BoolValue);
                 break;
             case ShaderProperty.PropertyType.Matrix:
-                shader.SetMatrix(property.Name, property.MatrixValue);
+                shader.SetMatrix(property.Id, property.MatrixValue);
                 break;
             case ShaderProperty.PropertyType.Texture:
-                shader.SetTexture(property.KernelIndex, property.Name, property.TextureValue);
+                shader.SetTexture(property.KernelIndex, property.Id, property.TextureValue);
                 break;
             default:
                 throw new ArgumentOutOfRangeException();
@@ -81,28 +96,28 @@ public static class ShaderPropertyExtensions
     {
         switch (property.Type) {
             case ShaderProperty.PropertyType.Float:
-                material.SetFloat(property.Name, property.FloatValue);
+                material.SetFloat(property.Id, property.FloatValue);
                 break;
             case ShaderProperty.PropertyType.Int:
-                material.SetInt(property.Name, property.IntValue);
+                material.SetInt(property.Id, property.IntValue);
                 break;
             case ShaderProperty.PropertyType.Vector:
-                material.SetVector(property.Name, property.VectorValue);
+                material.SetVector(property.Id, property.VectorValue);
                 break;
             case ShaderProperty.PropertyType.Color:
-                material.SetColor(property.Name, property.ColorValue);
+                material.SetColor(property.Id, property.ColorValue);
                 break;
             case ShaderProperty.PropertyType.Matrix:
-                material.SetMatrix(property.Name, property.MatrixValue);
+                material.SetMatrix(property.Id, property.MatrixValue);
                 break;
             case ShaderProperty.PropertyType.Texture:
-                material.SetTexture(property.Name, property.TextureValue);
+                material.SetTexture(property.Id, property.TextureValue);
                 break;
             case ShaderProperty.PropertyType.TextureScale:
-                material.SetTextureScale(property.Name, property.TextureOffsetValue);
+                material.SetTextureScale(property.Id, property.TextureOffsetValue);
                 break;
             case ShaderProperty.PropertyType.TextureOffset:
-                material.SetTextureOffset(property.Name, property.TextureScaleValue);
+                material.SetTextureOffset(property.Id, property.TextureScaleValue);
                 break;
             default:
                 throw new ArgumentOutOfRangeException();
