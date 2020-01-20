4fb39f7418380d5c0aac368831672db5e3b26fff
Lachlan Sleight <lachlansleight@gmail.com>
2020-01-20 13:24:27 +1100

    Added TransformExtensions (very limited functionality atm)

---
+++ Scripts/TransformExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lunity
+{
+    public static class TransformExtensions
+    {
+        public static void SetPositionX(this Transform tr, float newX, Space space = Space.World)
+        {
+            var u = space == Space.World ? tr.position : tr.localPosition;
+            u.x = newX;
+            if(space == Space.World) tr.position = u;
+            else tr.localPosition = u;
+        }
+        
+        public static void IncrementPositionX(this Transform tr, float increment, Space space = Space.World)
+        {
+            var u = space == Space.World ? tr.position : tr.localPosition;
+            u.x += increment;
+            if(space == Space.World) tr.position = u;
+            else tr.localPosition = u;
+        }
+        
+        public static void SetPositionY(this Transform tr, float newY, Space space = Space.World)
+        {
+            var u = space == Space.World ? tr.position : tr.localPosition;
+            u.y = newY;
+            if(space == Space.World) tr.position = u;
+            else tr.localPosition = u;
+        }
+        
+        public static void IncrementPositionY(this Transform tr, float increment, Space space = Space.World)
+        {
+            var u = space == Space.World ? tr.position : tr.localPosition;
+            u.y += increment;
+            if(space == Space.World) tr.position = u;
+            else tr.localPosition = u;
+        }
+        
+        public static void SetPositionZ(this Transform tr, float newZ, Space space = Space.World)
+        {
+            var u = space == Space.World ? tr.position : tr.localPosition;
+            u.z = newZ;
+            if(space == Space.World) tr.position = u;
+            else tr.localPosition = u;
+        }
+        
+        public static void IncrementPositionZ(this Transform tr, float increment, Space space = Space.World)
+        {
+            var u = space == Space.World ? tr.position : tr.localPosition;
+            u.z += increment;
+            if(space == Space.World) tr.position = u;
+            else tr.localPosition = u;
+        }
+    }
+}
