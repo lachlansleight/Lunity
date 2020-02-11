6c09384a175f7d9b4200cabdd46d64d6b73d1145
Lachlan Sleight <lachlansleight@gmail.com>
2020-02-11 11:16:04 +1100

    Added IInitializable interface

---
+++ Scripts/Interfaces/IInitializable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lunity
+{
+    public interface IInitializable
+    {
+        void Initialize();
+        bool Initialized { get; set; }
+    }
+}
