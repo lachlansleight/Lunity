5780932f9d080d52e4b019f5dbcbe4c9809a18cb
Lachlan Sleight <lachlansleight@gmail.com>
2020-02-20 17:24:56 +1100

    Added AutoTransform

---
+++ Scripts/AutoTransform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class AutoTransform : MonoBehaviour
+{
+    public Vector3 PositionOffsetPerSecond;
+    public Vector3 RotationOffsetPerSecond;
+    public Vector3 ScaleOffsetPerSecond;
+
+    public Space TransformSpace = Space.Self;
+
+    public void Update()
+    {
+        transform.Translate(PositionOffsetPerSecond * Time.deltaTime, TransformSpace);
+        transform.Rotate(RotationOffsetPerSecond * Time.deltaTime, TransformSpace);
+        transform.localScale += ScaleOffsetPerSecond * Time.deltaTime;
+    }
+}
