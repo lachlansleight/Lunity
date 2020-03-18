a331093d0ce38e2442730c3e4cd0c19f895f4a35
Lachlan Sleight <lachlansleight@gmail.com>
2020-03-18 14:13:59 +1100

    Added ComponentToggle script

---
+++ Scripts/ComponentToggle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentToggle : MonoBehaviour
+{
+    public Behaviour TargetBehaviour;
+    public KeyCode ToggleKey = KeyCode.ScrollLock;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(ToggleKey)) TargetBehaviour.enabled = !TargetBehaviour.enabled;
+    }
+}
