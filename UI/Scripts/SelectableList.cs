05a07df10a071c74948e0c11f1262cad1c9c9c85
Lachlan Sleight <lachlansleight@gmail.com>
2020-07-21 20:14:08 +1000

    Added ItemCount to selectable list (and added it to namespace)

--- UI/Scripts/SelectableList.cs
+++ UI/Scripts/SelectableList.cs
@@ -5,112 +5,118 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(LayoutGroup))]
-public class SelectableList : MonoBehaviour
+namespace Lunity
 {
-    public class SelectableItemData
-    {
-        public string Label;
-        public object Data;
 
-        public SelectableItemData(string label, object data)
+    [RequireComponent(typeof(LayoutGroup))]
+    public class SelectableList : MonoBehaviour
+    {
+        public class SelectableItemData
         {
-            Label = label;
-            Data = data;
+            public string Label;
+            public object Data;
+
+            public SelectableItemData(string label, object data)
+            {
+                Label = label;
+                Data = data;
+            }
         }
-    }
 
-    private class SelectableItem
-    {
-        public SelectableItemData Data;
-        public Button Button;
-
-        public SelectableItem(SelectableItemData data, Button button)
+        private class SelectableItem
         {
-            Data = data;
-            Button = button;
-        }
-    }
-    
-    
-    public EventHandler<object> OnItemSelected;
-    public SelectableItemData CurrentSelection;
-    
-    private RectTransform _itemTemplate;
-    private List<SelectableItem> Items;
-    private bool _initialized;
-    
-    public void Awake()
-    {
-        if (!_initialized) Initialize();
-    }
+            public SelectableItemData Data;
+            public Button Button;
 
-    public void Initialize()
-    {
-        if (_initialized) return;
-        
-        Items = new List<SelectableItem>();
-
-        _itemTemplate = transform.Find("Template").GetComponent<RectTransform>();
-        _itemTemplate.gameObject.SetActive(false);
-
-        _initialized = true;
-    }
-    
-    public void AddItem(string label, object data)
-    {
-        if(!_initialized) Initialize();
-        
-        if (Items.Any((item => item.Data == data))) {
-            Debug.LogError("Items list already contains " + data);
-            return;
+            public SelectableItem(SelectableItemData data, Button button)
+            {
+                Data = data;
+                Button = button;
+            }
         }
 
-        var newItemData = new SelectableItemData(label, data);
-        var newObj = Instantiate(_itemTemplate, transform, true).gameObject;
-        newObj.gameObject.SetActive(true);
-        newObj.gameObject.name = label;
-        var button = newObj.GetComponent<Button>();
-        if (button == null) button = newObj.GetComponentInChildren<Button>();
-        button.transform.GetChild(0).GetComponent<Text>().text = label;
-        Items.Add(new SelectableItem(newItemData, button));
-        var index = Items.Count - 1;
-        button.onClick.AddListener(() => HandleSelected(index));
-    }
 
-    public void ClearAllItems()
-    {
-        if(!_initialized) Initialize();
-        
-        foreach (var item in Items) {
-            Destroy(item.Button.gameObject);
+        public EventHandler<object> OnItemSelected;
+        public SelectableItemData CurrentSelection;
+        public int ItemCount => Items?.Count ?? 0;
+
+        private RectTransform _itemTemplate;
+        private List<SelectableItem> Items;
+        private bool _initialized;
+
+        public void Awake()
+        {
+            if (!_initialized) Initialize();
         }
 
-        Items.Clear();
-    }
-    
-    public void DeselectAll()
-    {
-        if(!_initialized) Initialize();
-        
-        foreach (var item in Items) item.Button.interactable = true;
+        public void Initialize()
+        {
+            if (_initialized) return;
 
-        CurrentSelection = null;
-    }
+            Items = new List<SelectableItem>();
 
-    private void HandleSelected(int index)
-    {
-        if(!_initialized) Initialize();
-        
-        if (index < 0 || index >= Items.Count) {
-            Debug.LogError($"Index of {index} doesn't fit in buttons list with count {Items.Count}!");
-            return;
+            _itemTemplate = transform.Find("Template").GetComponent<RectTransform>();
+            _itemTemplate.gameObject.SetActive(false);
+
+            _initialized = true;
         }
 
-        DeselectAll();
-        Items[index].Button.interactable = false;
-        CurrentSelection = Items[index].Data;
-        OnItemSelected?.Invoke(Items[index].Button, Items[index].Data);
+        public void AddItem(string label, object data)
+        {
+            if (!_initialized) Initialize();
+
+            if (Items.Any((item => item.Data == data))) {
+                Debug.LogError("Items list already contains " + data);
+                return;
+            }
+
+            var newItemData = new SelectableItemData(label, data);
+            var newObj = Instantiate(_itemTemplate, transform, true).gameObject;
+            newObj.gameObject.SetActive(true);
+            newObj.gameObject.name = label;
+            var button = newObj.GetComponent<Button>();
+            if (button == null) button = newObj.GetComponentInChildren<Button>();
+            button.transform.GetChild(0).GetComponent<Text>().text = label;
+            Items.Add(new SelectableItem(newItemData, button));
+            var index = Items.Count - 1;
+            button.onClick.AddListener(() => HandleSelected(index));
+        }
+
+        public void ClearAllItems()
+        {
+            if (!_initialized) Initialize();
+
+            foreach (var item in Items) {
+                Destroy(item.Button.gameObject);
+            }
+
+            Items.Clear();
+        }
+
+        public void DeselectAll()
+        {
+            if (!_initialized) Initialize();
+
+            foreach (var item in Items) item.Button.interactable = true;
+
+            CurrentSelection = null;
+        }
+
+        private void HandleSelected(int index)
+        {
+            if (!_initialized) Initialize();
+
+            if (index < 0 || index >= Items.Count) {
+                Debug.LogError($"Index of {index} doesn't fit in buttons list with count {Items.Count}!");
+                return;
+            }
+
+            DeselectAll();
+            Items[index].Button.interactable = false;
+            CurrentSelection = Items[index].Data;
+            OnItemSelected?.Invoke(Items[index].Button, Items[index].Data);
+        }
+
     }
 
 }
