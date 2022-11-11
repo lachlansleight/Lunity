b1c8c41de6baeb411399703ddb5ca691b91a0ae2
Lachlan Sleight <lachlansleight@gmail.com>
2022-11-11 11:32:22 +1100

    feat: Added randomize Y rotation to Transform context menu

--- Runtime/Scripts/TransformExtensions.cs
+++ Runtime/Scripts/TransformExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Lunity
 {
@@ -76,5 +79,14 @@ namespace Lunity
             tr.localRotation = Quaternion.identity;
             tr.localScale = Vector3.one;
         }
+        
+#if UNITY_EDITOR
+        [MenuItem("CONTEXT/Transform/Randomize Y Rotation")]
+        private static void RandomizeRotationY(MenuCommand command)
+        {
+            var t = (Transform) command.context;
+            t.localRotation = LRandom.FlatRotation();
+        }
+#endif
     }
 }
