0be0870922d75a5899319dfc57efe4b554717e73
lachlansleight <lachlansleight@gmail.com>
2024-09-09 11:30:54 +0100

    feat: Add editor screenshot

---
+++ Runtime/Scripts/EditorScreenshot.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lunity
+{
+    public class EditorScreenshot : MonoBehaviour
+    {
+        public bool StartFromDataPath = true;
+        public string Directory = "/Screenshots";
+        public KeyCode ScreenshotKey = KeyCode.F12;
+        [Range(1, 4)] public int Supersize = 1;
+
+        public void Update()
+        {
+            if (Input.GetKeyDown(ScreenshotKey)) {
+                var basePath = (StartFromDataPath ? Application.dataPath : "") + Directory;
+                ScreenCapture.CaptureScreenshot($"{basePath}/{DateTimeOffset.Now.ToUnixTimeMilliseconds()}.png", Supersize);
+            }
+        }
+    }
+}
