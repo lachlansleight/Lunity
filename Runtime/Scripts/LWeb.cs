171c3b0b0b6933b7ff63aa649f787269cd2a8d7b
lachlansleight <lachlansleight@gmail.com>
2024-11-15 11:59:11 +0000

    feat: Add byte array GET request to LWeb

--- Runtime/Scripts/LWeb.cs
+++ Runtime/Scripts/LWeb.cs
@@ -24,6 +24,18 @@ namespace Lunity
             }
         }
         
+        public static async Task<byte[]> GetBytesAsync(string uri, Action<string> onError = null)
+        {
+            Debug.Log("GET: " + uri);
+            var response = await Client.GetAsync(uri);
+            try {
+                return await response.Content.ReadAsByteArrayAsync();
+            } catch (HttpRequestException e) {
+                onError?.Invoke(e.Message);
+                return new byte[0];
+            }
+        }
+        
         public static async Task<string> PostAsync(string uri, string body, Action<string> onError = null)
         {
             Debug.Log("POST: " + uri + "\n" + body);
@@ -39,6 +51,13 @@ namespace Lunity
         public static IEnumerator GetCoroutine(string uri, Action<string> onResponse, Action<string> onError = null)
         {
             var task = GetAsync(uri, onError);
+            yield return new WaitUntil(() => task.IsCompleted);
+            onResponse?.Invoke(task.Result);
+        }
+        
+        public static IEnumerator GetBytesCoroutine(string uri, Action<byte[]> onResponse, Action<string> onError = null)
+        {
+            var task = GetBytesAsync(uri, onError);
             yield return new WaitUntil(() => task.IsCompleted);
             onResponse?.Invoke(task.Result);
         }
